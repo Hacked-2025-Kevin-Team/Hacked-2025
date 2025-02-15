8573132cddaf555b4017a0f4b70d0544656bc461
nguyentuthanhdn <44814087+nguyentuthanhdn@users.noreply.github.com>
2025-02-15 16:01:41 -0700

    dummy llm w tools

--- backend/src/llm/llm.py
+++ backend/src/llm/llm.py
@@ -1,8 +1,95 @@
 import os
-import configparser
 import dotenv
+import json
+
+from typing import Annotated
+from typing_extensions import TypedDict
+
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langchain_core.messages import ToolMessage
+
+from langchain_openai import ChatOpenAI
+from langchain_community.tools.tavily_search import TavilySearchResults
+
 
 dotenv.load_dotenv()
-print(os.getenv('OPENAI_API_KEY'))
+OPENAI_KEY = os.getenv('OPENAI_API_KEY')
+TAVILY_KEY = os.getenv('TAVILY_API_KEY')
 
 
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+
+class BasicToolNode:
+    """A node that runs the tools requested in the last AIMessage."""
+
+    def __init__(self, tools: list) -> None:
+        self.tools_by_name = {tool.name: tool for tool in tools}
+
+    def __call__(self, inputs: dict):
+        if messages := inputs.get("messages", []):
+            message = messages[-1]
+        else:
+            raise ValueError("No message found in input")
+        outputs = []
+        for tool_call in message.tool_calls:
+            tool_result = self.tools_by_name[tool_call["name"]].invoke(
+                tool_call["args"]
+            )
+            outputs.append(
+                ToolMessage(
+                    content=json.dumps(tool_result),
+                    name=tool_call["name"],
+                    tool_call_id=tool_call["id"],
+                )
+            )
+        return {"messages": outputs}
+
+
+
+class LLM:
+    def __init__(self):
+        self.graph_builder = StateGraph(State)
+               
+        tool = TavilySearchResults(max_results=2)
+        self.tools = [tool]
+        self.llm = ChatOpenAI(model="gpt-4o-mini")
+        self.llm_with_tools = self.llm.bind_tools(self.tools)
+        
+        self.graph_builder.add_node("chatbot", self.chatbot)
+        self.graph_builder.add_edge(START, "chatbot")
+        
+        self.tool_node = BasicToolNode(tools=[tool])
+        self.graph_builder.add_node("tools", self.tool_node)
+        self.graph_builder.add_conditional_edges("chatbot", self.route_tools, {"tools": "tools", END: END})
+        self.graph_builder.add_edge("tools", "chatbot")
+
+        self.graph = self.graph_builder.compile()
+        #self.graph.get_graph().draw_mermaid_png(output_file_path="test.png")
+        
+    def chatbot(self, state: State) -> State:
+        return {"messages": [self.llm_with_tools.invoke(state["messages"])]}
+    
+    
+    def stream_graph_updates(self, user_input: str):
+        for event in self.graph.stream({"messages": [{"role": "user", "content": user_input}]}):
+            for value in event.values():
+                print("Assistant:", value["messages"][-1].content)
+
+    def route_tools(self, state: State):
+        if isinstance(state, list):
+            ai_message = state[-1]
+        elif messages := state.get("messages", []):
+            ai_message = messages[-1]
+        else:
+            raise ValueError(f"No messages found in input state to tool_edge: {state}")
+        if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
+            return "tools"
+        return END
+
+
+user_input = "What do you know about LangGraph?"
+test = LLM()
+test.stream_graph_updates(user_input)
