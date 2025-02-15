20c331c72fae1208f6a3f12d4260b0cea1442e04
Logan <lgntravis@gmail.com>
2025-02-15 16:56:36 -0700

    Throwing cors into the most wretched pits of hell where it came from

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 import uvicorn
 from api import router as api_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "https://hacked-2025-backend-production.up.railway.app"],  # Add your frontend origin here
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router)
 
