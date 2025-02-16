cbaf1aef2c236c0002d57dbb55e1c6a18ee3cf32
Logan <lgntravis@gmail.com>
2025-02-16 13:38:39 -0700

    Consulted scientists after decades of research to learn how to center a div

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -86,8 +86,8 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-100 dark:bg-gray-800">
-          <div className="container px-4 md:px-6">
+        <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-100 dark:bg-gray-800 items-center flex flex-col">
+          <div className="container px-4 md:px-6 items-center">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-center mb-8">
               Latest Research Papers
             </h2>
