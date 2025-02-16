149954749b834cae36efd1d7393c93b035765813
Logan <lgntravis@gmail.com>
2025-02-15 19:08:40 -0700

    ALL ORIGINS

--- backend/src/main.py
+++ backend/src/main.py
@@ -9,11 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://hacked-2025-backend-production.up.railway.app",
-        "https://hacked-2025-production.up.railway.app",
-    ],  # Add your frontend origin here
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
