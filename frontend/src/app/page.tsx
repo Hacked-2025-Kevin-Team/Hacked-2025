06129c72310c1a34e9ae185f989cff20d17d8243
Logan Travis <lgntravis@gmail.com>
2025-02-17 00:45:28 -0700

    brody cooked up some good solutions for me

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -16,8 +16,6 @@ export default function Home() {
     setResponse("");
 
     try {
-      console.log("NEXT_PUBLIC_BASE_URL:", process.env.NEXT_PUBLIC_BASE_URL);
-
       const res = await fetch(
         `${process.env.NEXT_PUBLIC_BASE_URL}/chat-stream?usr_input=${encodeURIComponent(input)}`,
       );
