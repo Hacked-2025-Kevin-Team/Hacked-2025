1cbb5830c2b04cd8966467df82808e24bc4af2b6
Logan Travis <lgntravis@gmail.com>
2025-02-17 01:19:29 -0700

    Allow all origins

--- backend/src/main.py
+++ backend/src/main.py
@@ -11,8 +11,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://hacked-2025-backend-production.up.railway.app", "https://hacked-2025-production.up.railway.app"],  
-    allow_credentials=True,
+    allow_origins=["*"],  
     allow_methods=["*"],  
     allow_headers=["*"],  
 )
